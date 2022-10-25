bfc64fbb8de9a3e1825e9371e9e9d2a5a6699a7c
dear-ore <adorable2209@gmail.com>
2022-10-25 18:58:41 +0100

    improved script.js for ERC20 interaction

--- Week5/script.js
+++ Week5/script.js
@@ -1,13 +1,11 @@
 const { ethers } = require("ethers");
+require("dotenv").config();
 
-const rpcProvider = new ethers.providers.JsonRpcProvider("https://eth-goerli.g.alchemy.com/v2/VlnGYOxJ5P_t_UGpT-ThXoPszJ4E_gSw");
-//const rpcSigner = rpcProvider.getSigner();
 
-/*const main = async() => {
-	let blockNum;
-	blockNum = await rpcProvider.getBlockNumber();
-	console.log(blockNum);
-}*/
+const rpcProvider = new ethers.providers.JsonRpcProvider(process.env.myAlchemyURL);
+const privateKey = process.env.myPrivateKey;
+
+
 const tokenAddress = "0xd56083a53553b495a7ec0c5a8481fe6b81dc3b61";
 const tokenABI = [
 	"function name() view returns (string)",
@@ -19,17 +17,31 @@ const tokenABI = [
 ]
 
 const tokenContract = new ethers.Contract(tokenAddress, tokenABI, rpcProvider);
-const main = async() => {
-	let tokenName = await tokenContract.name();
-	console.log(tokenName);
+const myWallet = new ethers.Wallet(privateKey, rpcProvider);
 
-	let tokenSymbol = await tokenContract.symbol();
-	console.log(tokenSymbol);
 
-	let totalSupply = await tokenContract.balanceOf("0xd3fa22780f4b73564044dd9f6b4529812cc15306");
-	ethers.utils.formatUnits(totalSupply, 18)
-	console.log(totalSupply);
-
+const testTransfer = async() => {
+	const tokenWithWallet = tokenContract.connect(myWallet);
+	const tx = await tokenWithWallet.transfer("0x24298982ebc02AFf67349482eC1FF8987619b433", 40000);
+	const balance = await tokenWithWallet.balanceOf("0x24298982ebc02AFf67349482eC1FF8987619b433");
+	console.log(balance);
 }
 
-main();
+
+const getTokenName = async() => {
+	let tokenName = await tokenContract.name();
+	console.log(tokenName);
+}
+	
+const getTokenSymbol = async() => {
+	let tokenSymbol = await tokenContract.symbol();
+	console.log(tokenSymbol);
+}
+
+const getTotalSupply = async() => {
+	let totalSupply = await tokenContract.totalSupply();
+	console.log(totalSupply);
+}
+
+getTokenName();
+testTransfer();
